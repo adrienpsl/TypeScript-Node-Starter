b421138504074c3a3563497cfa9a1c921786263e
adpusel <adrien.pusel@gmail.com>
2020-05-26 09:40:50 +0200

    api start :)

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,33 +1,31 @@
-//import { Request, Response } from 'express';
-//
-///**
-// * GET /
-// * Home page.
-// */
-//export const index = ( req: Request, res: Response ) => {
-//  const { user } = req;
-//
-//  if ( !user ) {
-//    return res.redirect( '/login' );
-//  }
-//
-//  //@ts-ignore
-//  if ( !user.founderForm ) {
-//    return res.redirect( '/founderForm' );
-//  }
-//
-//  res.render( 'founderBrowse/start', {
-//    title : 'What do you whant'
-//  } );
-//};
-//
-///**
-// * GET /
-// * Home page.
-// */
-//export const test = ( req: Request, res: Response ) => {
-//  console.log( req.params );
-//  res.render( 'airtableForm/founderForm', {
-//    title : 'Home'
-//  } );
-//};
+import { Request, Response } from 'express';
+
+/**
+ * GET /
+ * Home page.
+ */
+export const index = ( req: Request, res: Response ) => {
+  const { user } = req;
+
+  //if ( !user ) {
+  //  return res.redirect( '/login' );
+  //}
+
+  //res.render( 'founderBrowse/start', {
+  //  title : 'What do you whant'
+  //} );
+
+  res.send( 'bite' );
+};
+
+/**
+ * GET /
+ * Home page.
+ */
+export const test = ( req: Request, res: Response ) => {
+  console.log( req.params );
+  res.render( 'airtableForm/founderForm', {
+    title : 'Home'
+  } );
+};
+
